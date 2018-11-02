e174fa352d1970a896883cc3859b53ae1fc00d91
tkocher621 <42099068+tkocher621@users.noreply.github.com>
2018-11-01 20:17:05 -0400

    Update StringExplorer.java

--- src/StringExplorer.java
+++ src/StringExplorer.java
@@ -22,6 +22,18 @@ public class StringExplorer
 		
 		//  Try other methods here:
 
+        int notFoundPsn = sample.indexOf("slow");
+        System.out.println("sample.indexOf(\"slow\") = " + notFoundPsn);
+
+        int notFoundPsn1 = sample.indexOf("quick");
+        System.out.println("sample.indexOf(\"slow\") = " + notFoundPsn1);
+        // Starts from the beginning of the string, so it will find the word
+
+        int notFoundPsn2 = sample.indexOf("quick", 5);
+        System.out.println("sample.indexOf(\"slow\") = " + notFoundPsn2);
+        //Starts from the fifth index in the string, which is past the full word of 'quick', so it wont find it
+
+
 	}
 
 }
